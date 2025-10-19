938a4fa809f4a5f175a19c043c00c63adaa732b8
Mike Lightner <fuzztooth@gmail.com>
2025-10-19 11:36:26 -0500

    Commit software as a whole (Part 2)

--- scripts/config-generator.sh
+++ scripts/config-generator.sh
@@ -3,26 +3,29 @@
 ENV=$1
 REGION=$2
 SECRET=$3
-DRUPAL_HASH_SALT=$4
-DRUPAL_CONF_OVERRIDES=$5
-DOMAIN=$6
+WIKI_CONF_OVERRIDES=$4
+DOMAIN=$5
 
 # JQ Note: Values from secretsmanager are piped through JQ twice in case the
 # returned format is not properly formatted json (jq will cleanup each level).
-# Values in $DRUPAL_CONF_OVERRIDES are expected to be properly formatted json.
+# Values in $WIKI_CONF_OVERRIDES are expected to be properly formatted json.
 
 cat << EOF
+MYSQL_PORT=`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.port'`
 MYSQL_DATABASE=`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.dbname'`
 MYSQL_HOSTNAME=`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.host'`
-MYSQL_PASSWORD="`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.password'`"
-MYSQL_PORT=`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.port'`
 MYSQL_USERNAME="`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.username'`"
-HASH_SALT="$DRUPAL_HASH_SALT"
-SMTP_HOST=`echo $DRUPAL_CONF_OVERRIDES | jq -r '.smtp_settings.smtp_host'`
-SMTP_PORT=`echo $DRUPAL_CONF_OVERRIDES | jq -r '.smtp_settings.smtp_port'`
-SMTP_USERNAME="`echo $DRUPAL_CONF_OVERRIDES | jq -r '.smtp_settings.smtp_username'`"
-SMTP_PASSWORD="`echo $DRUPAL_CONF_OVERRIDES | jq -r '.smtp_settings.smtp_password'`"
-TFA_KEY="`echo $DRUPAL_CONF_OVERRIDES | jq -r '.key_key_tfa.key_provider_settings.key_value'`"
+MYSQL_PASSWORD="`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.password'`"
+MYSQL_ADMINUSERNAME="`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.username'`"
+MYSQL_ADMINPASSWORD="`aws secretsmanager get-secret-value --secret-id $SECRET | jq -r '.SecretString' | jq -r '.password'`"
+SMTP_HOST=`echo $WIKI_CONF_OVERRIDES | jq -r '.smtp_settings.smtp_host'`
+SMTP_PORT=`echo $WIKI_CONF_OVERRIDES | jq -r '.smtp_settings.smtp_port'`
+SMTP_USERNAME="`echo $WIKI_CONF_OVERRIDES | jq -r '.smtp_settings.smtp_username'`"
+SMTP_PASSWORD="`echo $WIKI_CONF_OVERRIDES | jq -r '.smtp_settings.smtp_password'`"
+WIKI_SECRET_KEY="`echo $WIKI_CONF_OVERRIDES | jq -r '.wiki_secret_key'`"
+WIKI_UB_UPLOAD_BLACKLIST="`echo $WIKI_CONF_OVERRIDES | jq -r '.wiki_ub_upload_blacklist'`"
+WIKI_MONACO_PAYPAL_ID="`echo $WIKI_CONF_OVERRIDES | jq -r '.wiki_monaco_paypal_id'`"
+WIKI_GOOGLE_SITE_VERIFICATION="`echo $WIKI_CONF_OVERRIDES | jq -r '.wiki_google_site_verification'`"
 APP_DOMAIN='$DOMAIN'
 APP_ENV='$ENV'
 EOF
